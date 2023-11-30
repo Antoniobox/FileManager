0631df43a48a2a592e7d21b35fed9bea7d7fd1e3
Antoniobox <antonioboxsanchez@gmail.com>
2023-11-30 07:11:24 +0100

    Encapsulamiento de preload

--- preload.js
+++ preload.js
@@ -1,21 +1,28 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const fs = require('node:fs')
-    const divNombres = document.getElementById('listadoNombres')
-    const divTamanos = document.getElementById('listadoTamano')
-    fs.readdir('archivos/', (err, archivos)=>{
-        for(var archivo of archivos){
-            var elementoArchivo = document.createElement('h3')
-            elementoArchivo.innerHTML = archivo
-            divNombres.appendChild(elementoArchivo)
-            fs.stat('archivos/'+archivo, (err, stats)=>{
-                if(err) {
-                    alert('No se ha podido leer el archivo')
-                } else {
-                    var elementoTamano = document.createElement('h3')
-                    elementoTamano.innerHTML=stats.size + "B"
-                    divTamanos.appendChild(elementoTamano)
-                }
-            })
-        }
+/**
+ * Recoge la información de los archivos del directorio archivos
+ */
+function generarInformacionArchivos() {
+    window.addEventListener('DOMContentLoaded', () => {
+        const fs = require('node:fs')
+        const divNombres = document.getElementById('listadoNombres')
+        const divTamanos = document.getElementById('listadoTamano')
+        fs.readdir('archivos/', (err, archivos)=>{
+            for(var archivo of archivos){
+                var elementoArchivo = document.createElement('h3')
+                elementoArchivo.innerHTML = archivo
+                divNombres.appendChild(elementoArchivo)
+                fs.stat('archivos/'+archivo, (err, stats)=>{
+                    if(err) {
+                        alert('No se ha podido leer el archivo')
+                    } else {
+                        var elementoTamano = document.createElement('h3')
+                        elementoTamano.innerHTML=stats.size + "B"
+                        divTamanos.appendChild(elementoTamano)
+                    }
+                })
+            }
+        })
     })
-})
+}
+
+generarInformacionArchivos()
