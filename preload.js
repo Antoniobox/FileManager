f76f5f8aefd067a37f33d0cc8c65648a76e54170
Antoniobox <antonioboxsanchez@gmail.com>
2023-11-30 06:42:13 +0100

    Se agrega tamaño a la informacion

--- preload.js
+++ preload.js
@@ -1,11 +1,23 @@
+const { error } = require('node:console')
+
 window.addEventListener('DOMContentLoaded', () => {
     const fs = require('node:fs')
-    const div = document.getElementById('listado')
+    const divNombres = document.getElementById('listadoNombres')
+    const divTamanos = document.getElementById('listadoTamano')
     fs.readdir('archivos/', (err, archivos)=>{
         for(var archivo of archivos){
             var elementoArchivo = document.createElement('h3')
             elementoArchivo.innerHTML = archivo
-            div.appendChild(elementoArchivo)
+            divNombres.appendChild(elementoArchivo)
+            fs.stat('archivos/'+archivo, (err, stats)=>{
+                if(err) {
+                    alert('No se ha podido leer el archivo')
+                } else {
+                    var elementoTamano = document.createElement('h3')
+                    elementoTamano.innerHTML=stats.size + "B"
+                    divTamanos.appendChild(elementoTamano)
+                }
+            })
         }
     })
 })
